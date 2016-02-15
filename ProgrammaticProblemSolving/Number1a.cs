e74e566b27664e216ff433b9c510ba3ba5105365
Gabriel Nehemias <genehemias@gmail.com>
2016-02-15 16:25:27 -0500

    add two new methods to solve problem #1

---
+++ ProgrammaticProblemSolving/Number1a.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammaticProblemSolving
+{
+    class Number1A
+    {
+        //This seems more 'right'
+        public int RecursiveFindLCM(int first, int last)
+        {
+            bool thereYet = true;
+
+            for (int i = last; i >= 1; i--)
+            {
+                while (first%i != 0)
+                {
+                    first++;
+                    thereYet = false;
+                }
+            }
+
+            if (!thereYet)
+            {
+                first = RecursiveFindLCM(first, last);
+            }
+
+            return first;
+        }
+
+        //but this one can do numbers greater than 12!
+        public long IterativeFindLCM(long first, long last)
+        {
+            bool solvedYet = false;
+
+            while (!solvedYet)
+            {
+                solvedYet = true;
+                for (long i = last; i >= 1; i--)
+                {
+                    while (first%i != 0)
+                    {
+                        first++;
+                        solvedYet = false;
+                    }
+                }
+            }
+            return first;
+        }
+    }
+}
--- ProgrammaticProblemSolving/Program.cs
+++ ProgrammaticProblemSolving/Program.cs
@@ -12,9 +12,13 @@ namespace ProgrammaticProblemSolving
         static void Main(string[] args)
         {
             Number1 LCM = new Number1();
-            
+            Number1A ImprovedLCM = new Number1A();
+
             Console.WriteLine("The smallest positive number that is evenly divisible by all of the numbers from 1 to 12 is:");
-            Console.WriteLine(LCM.FindLcm());
+            Console.WriteLine("First attempt: " + LCM.FindLcm());
+            Console.WriteLine("Recursive solution: " + ImprovedLCM.RecursiveFindLCM(1, 12));
+            Console.WriteLine("Iterative solution: " + ImprovedLCM.IterativeFindLCM(1, 12));
+            
             Number2 MultipleSum = new Number2();
 
             Console.WriteLine("The sum of all the multiples of 3 or 5 below 1000 is:");
